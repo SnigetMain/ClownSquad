676828ba19a7b20d4e684252ee9b57e8ff4e3712
SnigetMain <v.gontar@uniyar.ac.ru>
2024-05-25 17:42:01 +0300

    s

---
+++ courier.py
@@ -0,0 +1,20 @@
+from worker import Worker,WorkerStatus
+from Order import Order
+from Order import Status
+class Courier(Worker):
+
+
+    def take_order(self,order:Order):
+        if(self.workerStatus == WorkerStatus.FREE):
+            self.workerStatus = WorkerStatus.INPROGRESSWORK
+            self.currentWork = order
+            order.ChangeStatus(Status.ONTHEWAY)
+            print(f'Доставляет заказ по адресу {order.address}')
+            self.giveOrder()
+
+    def giveOrder(self):
+        self.currentWork.status = Status.INPOINT
+        self.workerStatus = WorkerStatus.FREE
+        print(f'Курьер ДОставил заказ по адресу {self.currentWork.address}')
+        self.currentWork = None 
+
