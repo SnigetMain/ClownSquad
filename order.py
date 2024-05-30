3c73a35f3eb92d9127fea57d5e2d024ac77f0c1a
Andrew Golovkin <128581489+use700140@users.noreply.github.com>
2024-05-30 11:51:33 +0300

    Update order.py
    Добавил методы:
    1) Рассчитывать время готовки заказа
    2) Рассчитывать время доставки заказа курьером

--- order.py
+++ order.py
@@ -4,32 +4,53 @@ from product import Product
 # from typing import List
 import uuid
 
-class Status(Enum):
-    PROCESSING = 0 #обработка
-    ACCEPTED = 1  # приянт
-    INSTOCK = 2 #приготовление
-    ONTHEWAY = 3 #в пути
-    INPOINT = 4 # в пунткте
-    RECEIVED = 5 #получен
-    CANCEL = 6 #отменен
+from dataclasses import dataclass
+from enum import Enum
+from product import Product
+# from typing import List
+import uuid
 
-@dataclass
+
+class Status(Enum):
+    PROCESSING = 0  # обработка
+    ACCEPTED = 1  # приянт
+    INSTOCK = 2  # приготовление
+    ONTHEWAY = 3  # в пути
+    INPOINT = 4  # в пунткте
+    RECEIVED = 5  # получен
+    CANCEL = 6  # отменен
+
+
 class Order:
     orderId: str
     status: Status
     ProductList: list[Product]
-    timeCreation: str
+    timeCreation: float
     address: str
-    #collector: User
-    
 
-    def __init__(self,  status: Status=0, ProductList: list[Product]=[], adress="", timeCreation = 0):
+    # collector: User
+
+    def __init__(self, status: Status = 0, ProductList: list[Product] = [], adress="", timeCreation=0):
         self.orderId = uuid.uuid1()
         self.status = status
         self.ProductList = ProductList
         self.timeCreation = timeCreation
         self.address = adress
-        #self.collector = collector
+        # self.collector = collector
 
-    def ChangeStatus(self,status:Status):
+    def change_status(self, status: Status):
         self.status = status
+
+    # расчитать время готовки всех продуктов в минутах
+    def count_product_time(self):
+        for product in self.ProductList:
+            self.timeCreation += product.cooking_time
+        return self.timeCreation
+
+    # расчитать время доставки продуктов курьером
+    def count_courier_time(self, facility_position: tuple = (0, 0), client_position: tuple = (0, 0), courier_speed = 5):
+        #Расстояние через евклидову метрику
+        distance = ((facility_position[0] - client_position[0]) ** 2) + ((facility_position[1] - client_position[1]) ** 2) ** 0.5
+        return (distance / courier_speed * 60)
+
+    
